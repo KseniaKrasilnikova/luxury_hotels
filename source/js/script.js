778e99650c0c08adea50ebb510b3aa2817cb84b4
KseniaKrasilnikova <tksenia13@gmail.com>
2021-03-10 15:52:13 +0200

    done pagination script

--- source/js/script.js
+++ source/js/script.js
@@ -9,12 +9,18 @@ var reviewData = [
         'author': 'Mr. and Mrs. Baxter, UK'
     },
     {
-        'review': '"hahaha"',
+        'text': '"hahaha"',
         'author': 'Mr. and Mr. Smith, USA'
+    },
+    {
+        'text': '"Best breakfasts I ever tried and softest beds"',
+        'author': 'Mr. Yang, China'
     }
 ]
 
 var currentReview = 0;
+var lastReview = reviewData[reviewData.length - 1];
+var firstReview = reviewData[0];
 
 var fillReview = function (review) {
     reviewText.textContent = review.text;
@@ -27,7 +33,20 @@ var showNext = function () {
     prevBtn.classList.remove("disabled");
     currentReview++;
     fillReview(reviewData[currentReview]);
-}
+    if (reviewData[currentReview] == lastReview) {
+        nextBtn.classList.add('disabled');
+    }
+};
+
+var showPrev = function () {
+    nextBtn.classList.remove("disabled");
+    currentReview--;
+    fillReview(reviewData[currentReview]);
+    if (reviewData[currentReview] == firstReview) {
+        prevBtn.classList.add('disabled');
+    }
+};
 
 
 nextBtn.addEventListener('click', showNext, true);
+prevBtn.addEventListener('click', showPrev, true);
